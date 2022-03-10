442d206238ee0b57d286aa54572c0cba2156abb0
Jimp <2632697420@qq.com>
2022-03-10 09:32:48 +0800

    update !freelist

--- DbgExt.cpp
+++ DbgExt.cpp
@@ -5,24 +5,24 @@ DECLARE_API(chunk)
 {
 	if (!GetExpression(args))
 	{
-		dprintf("usage: !chunk <chunk_header addr>\n");
+		dprintf("usage: !chunk <chunk header addr>\n");
 		return;
 	}
 
-	ULONG64 chunk_addr = GetExpression(args);
-	ULONG64 heap_head = chunk_addr & ~0xffff;
+	ULONG64 chunk_head = GetExpression(args);
+	ULONG64 heap_head = chunk_head & ~0xffff;
 	
 	HEAP_ENTRY chunk = { 0 };
 	HEAP heap = { 0 };
-
 	ULONG bytesRead = 0;
+
 	if (!ReadMemory(heap_head, &heap, sizeof(HEAP), &bytesRead))
 	{
 		dprintf("[-] failed to read heap header\n");
 		return;
 	}
 
-	if (!ReadMemory(chunk_addr, &chunk, sizeof(HEAP_ENTRY), &bytesRead))
+	if (!ReadMemory(chunk_head, &chunk, sizeof(HEAP_ENTRY), &bytesRead))
 	{
 		dprintf("[-] failed to read chunk header\n");
 		return;
@@ -31,10 +31,11 @@ DECLARE_API(chunk)
 	// get the xor cookie
 	ULONG64 cookie = *(((PULONG64)&heap.Encoding) + 1);
 	dprintf("[+] cookie: 0x%I64x\n", cookie);
-	dprintf("[+] chunk:  0x%I64x\n", chunk_addr);
+	dprintf("[+] chunk:  0x%I64x\n", chunk_head);
 	
 	// decode the chunk header
 	*(((PULONG64)&chunk) + 1) ^= cookie;
+
 	dprintf("   [>] PreviousBlockPrivateData: 0x%I64x ", chunk.PreviousBlockPrivateData);
 	CHAR Data[9] = { 0 };
 	memcpy(Data, &chunk.PreviousBlockPrivateData, 8);
@@ -64,10 +65,101 @@ DECLARE_API(chunk)
 	dprintf("   [>] PreviousSize: 0x%x(%u)\n", chunk.PreviousSize << 4, chunk.PreviousSize << 4);
 }
 
+// command !freelist
+DECLARE_API(freelist)
+{
+	if (!GetExpression(args))
+	{
+		dprintf("usage: !freelist <heap header addr>\n");
+		return;
+	}
+
+	ULONG64 list_header = GetExpression(args) + 0x150;
+	dprintf("[+] list entry head: 0x%I64x\n", list_header);
+
+	LIST_ENTRY64 first_list_entry = { 0 };
+	ULONG bytesRead = 0;
+	
+	// first time get the flink and blink
+	if (!ReadMemory(list_header, &first_list_entry, sizeof(LIST_ENTRY64), &bytesRead))
+	{
+		dprintf("[-] failed to read list entry\n");
+		return;
+	}
+	dprintf("   [>] Flink: 0x%I64x ", first_list_entry.Flink);
+
+	LIST_ENTRY64 temp_list_entry = { 0 };
+	ULONG64 flink = first_list_entry.Flink;
+	int count = 2;
+	BOOL flag = FALSE;
+
+	while (ReadMemory(flink, &temp_list_entry, sizeof(LIST_ENTRY64), &bytesRead))
+	{
+		if (temp_list_entry.Flink == first_list_entry.Flink)
+		{
+			break;
+		}
+		dprintf("-> 0x%I64x ", temp_list_entry.Flink);
+		flink = temp_list_entry.Flink;
+
+		// beautify output
+		if (count % 6 == 0)
+		{
+			dprintf("\n           ");
+			flag = TRUE;
+		}
+		else
+		{
+			flag = FALSE;
+		}
+
+		++count;
+	}
+
+	if (!flag)
+	{
+		dprintf("\n");
+	}
+
+	dprintf("\n   [>] Blink: 0x%I64x ", first_list_entry.Blink);
+
+	temp_list_entry = { 0 };
+	ULONG64 blink = first_list_entry.Blink;
+	count = 2;
+
+	while (ReadMemory(blink, &temp_list_entry, sizeof(LIST_ENTRY64), &bytesRead))
+	{
+		if (temp_list_entry.Blink == first_list_entry.Blink)
+		{
+			break;
+		}
+		dprintf("-> 0x%I64x ", temp_list_entry.Blink);
+		blink = temp_list_entry.Blink;
+
+		if (count % 6 == 0)
+		{
+			dprintf("\n           ");
+			flag = TRUE;
+		}
+		else
+		{
+			flag = FALSE;
+		}
+		++count;
+	}
+
+	if (!flag)
+	{
+		dprintf("\n");
+	}
+
+}
+
 // command !heaphelp
 DECLARE_API(heaphelp)
 {
 	dprintf("[+] help for the windbgdll.dll\n");
-	dprintf("   [>] !chunk <addr>  - show the chunk header info\n");
-	dprintf("   [>] !heaphelp      - show the help message\n");
+	dprintf("   [>] !chunk    <addr>  - show the chunk header info\n");
+	dprintf("   [>] !freelist <addr>  - show the free list info\n");
+	dprintf("   [>] !heaphelp         - show the help message\n");
 }
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# !chunk
 ```shell
 1: kd> dq 2a08f180850 2a08f180958
 000002a0`8f180850  00000000`00000000 0800bbe9`d83e623f
@@ -18,5 +19,29 @@
    [>] Size: 0x100(256)
    [>] Flags: 0x0(free)
    [>] PreviousSize: 0x100(256)
+```
+
+# !freelist
+
+```shell
+1: kd> !heap 
+        Heap Address      NT/Segment Heap
+
+         1800ca50000              NT Heap
+         1800c830000              NT Heap
+         1800cca0000              NT Heap
+         1800ca10000              NT Heap
+1: kd> !freelist 1800ca10000
+[+] list entry head: 0x1800ca10150
+   [>] Flink: 0x1800ca11100 -> 0x1800ca166d0 -> 0x1800ca180e0 -> 0x1800ca10150 
+
+   [>] Blink: 0x1800ca180e0 -> 0x1800ca166d0 -> 0x1800ca11100 -> 0x1800ca10150 
+1: kd> !freelist 1800cca0000
+[+] list entry head: 0x1800cca0150
+   [>] Flink: 0x180113c9f50 -> 0x180113b5a00 -> 0x180113c9060 -> 0x180113c7e70 -> 0x1800ccaeb50 -> 0x180113c80d0 -> 0x180113b3e90 
+           -> 0x180113c8280 -> 0x180113b73d0 -> 0x180113c91a0 -> 0x1800cca0150 
+
+   [>] Blink: 0x180113c91a0 -> 0x180113b73d0 -> 0x180113c8280 -> 0x180113b3e90 -> 0x180113c80d0 -> 0x1800ccaeb50 -> 0x180113c7e70 
+           -> 0x180113c9060 -> 0x180113b5a00 -> 0x180113c9f50 -> 0x1800cca0150 
 ```
 
